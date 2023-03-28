5685d654d6d1e071614df4466007fccd3d8d16eb
BryantzTH <71428400+bryantz13@users.noreply.github.com>
2023-03-28 16:16:32 +0700

    update Shop

---
+++ components/cardShop/index.js
@@ -0,0 +1,125 @@
+import React from "react";
+import {
+  Flex,
+  Switch,
+  Text,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Box,
+  Spacer,
+  Button,
+  Wrap,
+  HStack,
+  WrapItem,
+  Select,
+  Center,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  useDisclosure,
+  Link,
+  Card,
+  CardBody,
+  Heading,
+} from "@chakra-ui/react";
+
+function index() {
+  return (
+    <>
+      <Card maxW="sm" mb={'10px'} boxShadow='base'>
+        <CardBody>
+          <Box>
+            <Flex alignItems="center">
+              <Switch colorScheme="brand" size="sm" mr={"5px"} />
+              เปิด/ปิดเพื่อแสดง
+              <Spacer />
+              <Image src="/images/star1.png" h="20px" w="20px" />
+            </Flex>
+          </Box>
+          <Box>
+            <Image
+              src="/images/YETI-1.jpg"
+              pos="relative"
+              mt={"5px"}
+              borderRadius="lg"
+              boxShadow="md"
+            />
+            <Image
+              src="/images/PM143.jpg"
+              pos="absolute"
+              top={"25%"}
+              left={"33%"}
+              borderRadius={"100%"}
+              width={"135px"}
+              height={"135px"}
+              border={"5px solid white"}
+            />
+          </Box>
+          <Box mt={"5px"}>
+            <Text fontSize="3xl" fontWeight={"bold"} textAlign="center">
+              แก้วเยติ ส่งไวทั่วไทย
+            </Text>
+            <Flex justifyContent={"space-around"}>
+              <Text fontSize="sm">จำนวนผู้เข้าชม : 5,980</Text>
+              <Text fontSize="sm">วันที่สร้าง : 04/09/64</Text>
+              <Text fontSize="sm">แก้ไขล่าสุด : 23/04/65</Text>
+            </Flex>
+          </Box>
+          <Box mt={"0.75rem"}>
+            <Flex justifyContent={"space-around"}>
+              <Button
+                bgColor={"white"}
+                border={"2px solid red"}
+                height={"30px"}
+              >
+                <Image
+                  src="/images/copyshop.png"
+                  width={"20px"}
+                  height={"20px"}
+                />
+                <Text ml={"8px"} fontSize="xl" color={"#ff0000"}>
+                  คัดลอกลิงค์
+                </Text>
+              </Button>
+              <Button
+                bgColor={"white"}
+                border={"2px solid black"}
+                height={"30px"}
+              >
+                <Image
+                  src="/images/binshop.png"
+                  width={"18px"}
+                  height={"18px"}
+                />
+                <Text ml={"8px"} fontSize="xl">
+                  ลบ
+                </Text>
+              </Button>
+              <Button
+                bgColor={"#ff0000"}
+                // border={"2px solid black"}
+                height={"30px"}
+              >
+                <Image
+                  src="/images/editshop.png"
+                  width={"18px"}
+                  height={"18px"}
+                />
+                <Text ml={"8px"} fontSize="xl" color={"white"}>
+                  แก้ไข
+                </Text>
+              </Button>
+            </Flex>
+          </Box>
+        </CardBody>
+      </Card>
+    </>
+  );
+}
+
+export default index;
--- pages/shop/index.js
+++ pages/shop/index.js
@@ -1,6 +1,116 @@
 import classNames from "classnames";
 import React from "react";
+import {
+  Flex,
+  Switch,
+  Text,
+  Image,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  Box,
+  Spacer,
+  Button,
+  Wrap,
+  HStack,
+  WrapItem,
+  Select,
+  Center,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  useDisclosure,
+  Link,
+  Card,
+  CardBody,
+  Heading,
+} from "@chakra-ui/react";
+import ListCheck from "@/components/MenuList";
+import CardShop from "@/components/cardShop";
 
 export default function shop() {
-  return <>shop</>;
+  const colunm = [
+    {
+      label: "เรียงตามตัวอักษร",
+    },
+    {
+      label: "เรียงตามวันที่สร้าง",
+    },
+    {
+      label: "เรียงตามวันที่แก้ไข",
+    },
+    {
+      label: "จำนวนผู้เข้าชม",
+    },
+  ];
+
+  return (
+    <>
+      <Box m="10px" pt="10px" pb={"10px"}>
+        <Flex>
+          <Box>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none" ml={"5px"}>
+                <Image src="/images/search.png" h="20px" w="20px" />
+              </InputLeftElement>
+              <Input
+                borderRadius="3xl"
+                type="text"
+                fontSize="21px"
+                borderColor="gray.500"
+                placeholder="ค้นหารายการ"
+              />
+            </InputGroup>
+          </Box>
+          <Box ml="10px">
+            <InputGroup>
+              <InputLeftElement pointerEvents="none" ml={"5px"}>
+                <Image src="/images/calendar.png" h="20px" w="20px" />
+              </InputLeftElement>
+              <Input
+                type="date"
+                borderRadius="3xl"
+                fontSize="21px"
+                borderColor="gray.500"
+                placeholder="เลือกวันที่"
+              />
+            </InputGroup>
+          </Box>
+          <Box ml="10px" border="1px" borderColor="black" borderRadius="md">
+            <ListCheck data={colunm} />
+          </Box>
+          <Spacer />
+          <Box borderWidth="1px" borderColor="red" borderRadius="md">
+            <Link href="/stock/addProduct">
+              <Button
+                fontSize="21px"
+                leftIcon={
+                  <Image src="/images/pluswhite.png" h="15px" w="15px" />
+                }
+                bg="red"
+                variant="solid"
+                color="white"
+                _hover={{}}
+              >
+                สร้างร้านค้า
+              </Button>
+            </Link>
+          </Box>
+        </Flex>
+      </Box>
+      <Box pt="5px" pb={"5px"}>
+        <Flex flexWrap={'wrap'} justifyContent={"space-around"}>
+          <CardShop />
+          <CardShop />
+          <CardShop />
+          <CardShop />
+          <CardShop />
+          <CardShop />
+        </Flex>
+      </Box>
+    </>
+  );
 }
